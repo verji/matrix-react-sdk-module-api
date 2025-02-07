8adc47176d6ff98d275ab26dee8decee8fef03bd
Ståle Eikebråten <eiksta@verji.com>
2025-02-07 15:06:32 +0100

    Added Copyright message

--- src/extensions/EventSearchExtensions.ts
+++ src/extensions/EventSearchExtensions.ts
@@ -1,3 +1,18 @@
+/*
+Copyright 2023 Verji Tech AS
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import { ISearchResultsProjection } from "./EventSearchExtensionProjections";
 
 export interface MatrixClientProjection {
@@ -30,8 +45,8 @@ export abstract class EventSearchExtensionsBase implements ProvideEventSearchExt
 
 /**
  *
- * The default/empty usersearch-extension
- * Can (and will) be used if none of the modules has an implementaion of ProvideUserSearchExtensions
+ * The default/empty eventsearch-extension
+ * Can (and will) be used if none of the modules has an implementaion of ProvideEventSearchExtensions
  *
  * */
 export class DefaultEventSearchExtensions extends EventSearchExtensionsBase {
